1e16271a824147d76d725a5aa4c999adf35c0f9c
Trinigch <trinigch@gmail.com>
2024-10-15 09:16:02 -0500

    adding a first complete cli , Motorbike and Truck file

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -10,12 +10,12 @@ class Cli {
   // TODO: update the vehicles property to accept Truck and Motorbike objects as well
   // TODO: You will need to use the Union operator to define additional types for the array
   // TODO: See the AbleToTow interface for an example of how to use the Union operator
-  vehicles: (Car)[];
+  vehicles: (Car |Truck  | Motorbike )[];
   selectedVehicleVin: string | undefined;
   exit: boolean = false;
 
   // TODO: Update the constructor to accept Truck and Motorbike objects as well
-  constructor(vehicles: (Car)[]) {
+  constructor(vehicles: (Car |Truck  | Motorbike)[]) {
     this.vehicles = vehicles;
   }
 
@@ -61,7 +61,7 @@ class Cli {
           name: 'vehicleType',
           message: 'Select a vehicle type',
           // TODO: Update the choices array to include Truck and Motorbike
-          choices: ['Car'],
+          choices: ['Car','Truck' , 'Motorbike'],
         },
       ])
       .then((answers) => {
@@ -70,6 +70,14 @@ class Cli {
           this.createCar();
         }
         // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
+        if (answers.vehicleType === 'Truck') {
+          // create a Truck
+          this.createTruck();
+        }
+        if (answers.vehicleType === 'Motorbike') {
+          // create a Motorbike
+          this.createMotorbike();
+        }
       });
   }
 
@@ -174,9 +182,23 @@ class Cli {
         // TODO: push the truck to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the truck
         // TODO: perform actions on the truck
+           const truck = new Truck(
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          [new Wheel(20, 'Goodyear'), new Wheel(20, 'Goodyear'), new Wheel(20, 'Goodyear'), new Wheel(20, 'Goodyear')],
+          parseInt(answers.towingCapacity)
+          
+        );
+        this.vehicles.push(truck);
+        this.selectedVehicleVin = truck.vin;
+        this.performActions();
       });
   }
-
   // method to create a motorbike
   createMotorbike(): void {
     inquirer
@@ -242,22 +264,29 @@ class Cli {
 
   // method to find a vehicle to tow
   // TODO: add a parameter to accept a truck object
-  findVehicleToTow(): void {
+  findVehicleToTow(truck:Truck): void {
     inquirer
       .prompt([
         {
           type: 'list',
           name: 'vehicleToTow',
           message: 'Select a vehicle to tow',
-          choices: this.vehicles.map((vehicle) => {
+          choices: this.vehicles
+          .filter((vehicle) => vehicle.vin !== truck.vin) 
+          .map((vehicle) => {
             return {
               name: `${vehicle.vin} -- ${vehicle.make} ${vehicle.model}`,
-              value: vehicle,
+              value: vehicle.vin,
             };
           }),
         },
       ])
       .then((answers) => {
+        const vehicleToTow = this.vehicles.find((vehicle) => vehicle.vin === answers.vehicleToTow);
+        if(vehicleToTow){
+          truck.tow(vehicleToTow);
+        }
+        this.performActions();        
         // TODO: check if the selected vehicle is the truck
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
         // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
@@ -282,6 +311,8 @@ class Cli {
             'Turn right',
             'Turn left',
             'Reverse',
+            'Tow vehicle', // adding Truck specific
+            'Perform wheelie', // adding  Motorbike specific
             'Select or create another vehicle',
             'Exit',
           ],
